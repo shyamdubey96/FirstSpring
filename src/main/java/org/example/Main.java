2316a2fb7b09d45785c6fa4ab5b2e6a297503f2c
Shyam dubey <shyam0853@gmail.com>
2023-08-19 14:07:18 +0530

    Main and Pom xml modified with applicationcontext

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+       Student stu = context.getBean("stu", Student.class);
+       stu.cheat();
     }
 }
